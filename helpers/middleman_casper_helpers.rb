f6bb7d88f500b30c8384db1f21f11dc667fbb0ee
Ryan Kretch <rkretch91@gmail.com>
2018-11-26 21:43:50 +0100

    eilat article added

--- helpers/middleman_casper_helpers.rb
+++ helpers/middleman_casper_helpers.rb
@@ -88,9 +88,9 @@ module MiddlemanCasperHelpers
   end
 
   # def covertag(page = current_page)
-  #   if (is_tag_page?)
+  #   if is_tag_page?
   #     current_resource.metadata[:locals]['page_type'] == 'tag'
-  #     { style: "background-image: url(#{tag}.jpg)" }
+  #     { style: "background-image: url(#{image_path(tag)}.jpg)" }
   #   else
   #     { class: 'no-cover' }
   #   end
